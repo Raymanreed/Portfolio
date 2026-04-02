c939b01f5022735759c64eb2d8ebd51d79a53ae3
Reed Gehling <raymanreed@gmail.com>
2026-04-02 14:22:49 -0700

    adding navigation

--- src/pages/projects/projectHome.tsx
+++ src/pages/projects/projectHome.tsx
@@ -8,6 +8,7 @@ function ProjectHome() {
                 <NavLink to="loops" className={"project-navigation"}>Loops and iteration demo's</NavLink>
                 <NavLink to="memorial"className={"project-navigation"}>RIP</NavLink>
                 <NavLink to="storyboard" className={"project-navigation"}>Chart a story!</NavLink>
+                <NavLink to="house" className={"project-navigation"}>Explore a house!</NavLink>
             </div>
         </div>
     )
